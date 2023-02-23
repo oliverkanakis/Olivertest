a50955bb4de19db86c0d8aa341883582d6d5eca9
Oliver Kanakis <112859607+oliverkanakis@users.noreply.github.com>
2023-02-23 10:55:33 +0100

    Replace var with const
    "Neva eva use var - Zivva"
    - VARför? - Ollan

--- script.js
+++ script.js
@@ -1,18 +1,18 @@
 function calculate() {
   // Get the selected quarter value
-  var quarterSelect = document.getElementById("quarter");
-  var quarterValue = quarterSelect.options[quarterSelect.selectedIndex].value;
+  const quarterSelect = document.getElementById("quarter");
+  const quarterValue = quarterSelect.options[quarterSelect.selectedIndex].value;
   
   // Get the total downtime value
-  var downtimeInput = document.getElementById("downtime");
-  var downtimeValue = downtimeInput.value;
+  const downtimeInput = document.getElementById("downtime");
+  const downtimeValue = downtimeInput.value;
   
   // Calculate the total uptime percentage
-  var totalMinutes = parseInt(quarterValue);
-  var totalDowntime = parseInt(downtimeValue);
-  var uptimePercentage = ((totalMinutes - totalDowntime) / totalMinutes) * 100;
+  const totalMinutes = parseInt(quarterValue);
+  const totalDowntime = parseInt(downtimeValue);
+  const uptimePercentage = ((totalMinutes - totalDowntime) / totalMinutes) * 100;
   
   // Display the result
-  var resultElement = document.getElementById("output");
+  const resultElement = document.getElementById("output");
   resultElement.innerHTML = "Total uptime: " + uptimePercentage.toFixed(2) + "%";
 }
