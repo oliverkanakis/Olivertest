c0ca8c95079d0cb53d3d1c972ffa453ee67d84e3
Oliver Kanakis <112859607+oliverkanakis@users.noreply.github.com>
2023-02-23 08:50:58 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,27 @@
+const quarterTime = {
+    Q1: 129600,
+    Q2: 131040,
+    Q3: 132480,
+    Q4: 132480
+};
 
+const calcForm = document.getElementById('calc-form');
+const quarterSelect = document.getElementById('quarter-select');
+const downtimeInput = document.getElementById('downtime-input');
+const resultDiv = document.getElementById('result');
+
+calcForm.addEventListener('submit', (event) => {
+    event.preventDefault(); // Prevent the default form submission behavior
+    
+    const selectedQuarter = quarterSelect.value;
+    const downtime = parseInt(downtimeInput.value, 10);
+    const totalQuarterTime = quarterTime[selectedQuarter];
+    
+    if (isNaN(downtime)) {
+        resultDiv.innerText = 'Please insert a valid downtime value.';
+        return;
+    }
+    
+    const uptimePercentage = (totalQuarterTime - downtime) / totalQuarterTime * 100;
+    resultDiv.innerText = `Quarter ${selectedQuarter}: ${uptimePercentage.toFixed(2)}% uptime`;
+});
