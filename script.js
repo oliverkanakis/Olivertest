0e0375b85f9a1715241d30fc74c64d0b14240a94
Oliver Kanakis <112859607+oliverkanakis@users.noreply.github.com>
2023-02-23 09:06:15 +0100

    Update script.js

--- script.js
+++ script.js
@@ -3,15 +3,16 @@ function calculate() {
   var quarterSelect = document.getElementById("quarter");
   var quarterValue = quarterSelect.options[quarterSelect.selectedIndex].value;
   
-  // Get the downtime value
+  // Get the total downtime value
   var downtimeInput = document.getElementById("downtime");
   var downtimeValue = downtimeInput.value;
   
-  // Calculate the result
-  var totalTime = parseInt(quarterValue);
-  var result = (totalTime - parseInt(downtimeValue)) / totalTime;
+  // Calculate the total uptime percentage
+  var totalMinutes = parseInt(quarterValue);
+  var totalDowntime = parseInt(downtimeValue);
+  var uptimePercentage = ((totalMinutes - totalDowntime) / totalMinutes) * 100;
   
   // Display the result
   var resultElement = document.getElementById("output");
-  resultElement.innerHTML = "Result: " + result.toFixed(2);
+  resultElement.innerHTML = "Total Uptime %: " + uptimePercentage.toFixed(2) + "%";
 }
